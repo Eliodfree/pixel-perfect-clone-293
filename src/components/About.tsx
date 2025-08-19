a729e8838ae2bb42d9a65fab3a948d821841e321
Muhangi2 <eliodcurry@gmail.com>
2025-08-19 13:05:31 +0300

    Refactor About component layout and styling; adjust spacing and text styles for improved readability and visual consistency.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const About: React.FC = () => {
   return (
-    <section className="box-border flex flex-col items-center gap-6 m-0 p-0 ">
-      <div className="box-border flex flex-col items-center gap-8 text-center m-0 p-0 ">
+    <section className="box-border flex flex-col items-center gap-2 m-0 p-0 ">
+      <div className="box-border flex flex-col items-center gap-4 text-center m-0 p-0 ">
         <h2 className="box-border text-[42px] font-extrabold leading-[42px] tracking-[0.42px] m-0 p-0 max-md:text-3xl max-sm:text-[24px]">
-          <span className="text-yellow-800">About </span>
-          <span className="bg-gradient-to-r from-yellow-600 via-yellow-400 to-yellow-700 bg-clip-text text-transparent">EthNile'25</span>
-        </h2>
+  <span className="text-yellow-800">About </span>
+  <span className="text-black font-black">EthNile'25</span>
+</h2>
+
         <p className="box-border text-black text-lg font-normal leading-[26px] m-0 p-0 max-md:text-base max-sm:text-sm">
           Get ready for history in the making. EthNile is the very first
           Ethereum-focused event in Kampala, a landmark moment for Uganda's
@@ -17,7 +18,7 @@ const About: React.FC = () => {
           connection, and boundless reach into the blockchain ecosystem.
         </p>
       </div>
-      <div className="box-border flex w-full items-stretch gap-4 m-0 p-0 flex-row flex-nowrap justify-between max-lg:gap-3 max-md:flex-col max-md:gap-4">
+      <div className="box-border flex w-full items-stretch gap-6 mt-4 p-0 flex-row flex-nowrap justify-between max-lg:gap-3 max-md:flex-col max-md:gap-4">
         <article className="box-border flex flex-col items-start gap-6 flex-1 border bg-[#FFF7D5] m-0 p-6 rounded-lg border-solid border-[rgba(72,59,0,0.50)] max-lg:p-5 max-md:p-5 max-sm:p-4">
           <div className="box-border flex flex-col items-start gap-4 m-0 p-0">
             <h3 className="box-border text-[#141100] text-[20px] font-bold leading-[24px] m-0 p-0 max-lg:text-lg max-md:text-lg max-sm:text-base">
